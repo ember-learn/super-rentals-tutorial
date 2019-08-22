7142b3b8b54e096cb673b3e485d8bd6a7a641232
Godfrey Chan <godfreykfc@gmail.com>
2019-08-21 20:33:17 -0700

    Setup auto deploy
    1. Push the markdown and assets to `ember-learn/guides-source#super-rentals-tutorial`
    2. Force push the code (with history) to `ember-learn/super-rentals#super-rentals-tutorial-output`
    3. Push the code (squashed) to `ember-learn/super-rentals#super-rentals-tutorial`

---
+++ scripts/deploy.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+# Setup SSH keys
+
+mkdir -p ~/.ssh
+chmod 700 ~/.ssh
+
+tar -xvzf deploy-keys.tar.gz -C ~/.ssh
+chmod 500 ~/.ssh/deploy-keys
+chmod 400 ~/.ssh/deploy-keys/*
+
+touch ~/.ssh/config
+chmod 600 ~/.ssh/config
+cat <<CONFIG >> ~/.ssh/config
+Host guides-source
+  HostName github.com
+  User git
+  IdentityFile ~/.ssh/deploy-keys/guides-source
+Host super-rentals
+  HostName github.com
+  User git
+  IdentityFile ~/.ssh/deploy-keys/super-rentals
+CONFIG
+
+# Setup tmpdir
+
+mkdir -p tmp
+pushd tmp
+
+# Guides
+
+git clone guides-source:ember-learn/guides-source --depth 1 -b super-rentals-tutorial
+pushd guides-source
+mkdir -p guides/release/tutorial/
+rm -rf guides/release/tutorial/*
+cp -r ../../dist/chapters/* guides/release/tutorial/
+git add guides/release/tutorial
+cp -r ../../dist/assets/* public/
+git add public
+cat <<COMMIT | git commit --allow-empty -F -
+Built from ember-learn/super-rentals-tutorial@$TRAVIS_COMMIT
+
+$TRAVIS_COMMIT_MESSAGE
+COMMIT
+git push
+popd
+
+# Code (with history)
+
+pushd ../dist/code/super-rentals
+git clean -dfx
+cat <<COMMIT | git commit --allow-empty -F -
+Built from ember-learn/super-rentals-tutorial@$TRAVIS_COMMIT
+
+$TRAVIS_COMMIT_MESSAGE
+COMMIT
+git remote add super-rentals super-rentals:ember-learn/super-rentals
+git push -f super-rentals master:super-rentals-tutorial-output
+popd
+
+# Code (without history)
+
+git clone super-rentals:ember-learn/super-rentals --depth 1 -b super-rentals-tutorial
+pushd super-rentals
+git rm -rf '*'
+cat <<COMMIT | git commit --allow-empty -F -
+Built from ember-learn/super-rentals-tutorial@$TRAVIS_COMMIT
+
+$TRAVIS_COMMIT_MESSAGE
+COMMIT
+git remote add dist ../../dist/code/super-rentals
+git fetch dist
+git merge --squash --no-commit --allow-unrelated-histories dist/master
+git commit --allow-empty --amend --no-edit
+git push
+popd
+
+# Cleanup
+
+popd
+
+rm -rf tmp
+rm -rf ~/.ssh/deploy-keys
