34a77733787bb21bfbd0c9c59cbc74f47d19e654
Godfrey Chan <godfreykfc@gmail.com>
2019-09-21 14:45:58 -0700

    Process retina images on a second pass

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,5 @@
 export { default as doNotEdit } from './plugins/do-not-edit';
+export { default as retinaImages } from './plugins/retina-images';
 export { default as runCodeBlocks } from './plugins/run-code-blocks';
 export { default as todoLinks } from './plugins/todo-links';
 export { default as zoeySays } from './plugins/zoey-says';
